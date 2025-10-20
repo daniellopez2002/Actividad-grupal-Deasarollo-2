1f0f4cd2b5d0d7b400859578007eec4d3872cf28
daniellopez2002 <89090856+daniellopez2002@users.noreply.github.com>
2025-10-19 19:24:27 -0500

    Last update & portal working

--- Assets/Scripts/PortalCamera.cs
+++ Assets/Scripts/PortalCamera.cs
@@ -5,24 +5,27 @@ public class PortalCamera : MonoBehaviour
     [Header("References")]
     public Camera playerCamera;
     public Transform portal;
-    public Transform linkedPortal;
 
+    private Transform _linkedPortal;
     private Camera _cam;
+    private PortalController _portalController;
 
     void Awake()
     {
         _cam = GetComponent<Camera>();
+        _portalController = GetComponentInParent<PortalController>();
+        _linkedPortal = _portalController.LinkedPortal.transform;
     }
 
     void LateUpdate()
     {
-        if (!playerCamera || !portal || !linkedPortal) return;
+        if (!playerCamera || !portal || !_linkedPortal) return;
 
         Quaternion relativeRot = Quaternion.Inverse(portal.rotation) * playerCamera.transform.rotation;
 
-        Quaternion finalRot = linkedPortal.rotation * relativeRot * Quaternion.Euler(0f, 180f, 0f);
+        Quaternion finalRot = _linkedPortal.rotation * relativeRot * Quaternion.Euler(0f, 180f, 0f);
         Vector3 euler = finalRot.eulerAngles;
-        transform.rotation = Quaternion.Euler(-euler.x, -euler.y, 0f);
+        transform.rotation = Quaternion.Euler(-euler.x, euler.y, 0f);
 
         _cam.fieldOfView = playerCamera.fieldOfView;
 
